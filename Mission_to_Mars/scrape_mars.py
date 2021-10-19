58b633342fe420598b8d860ff5f88fde94991a86
Nensan90 <85895812+Nensan90@users.noreply.github.com>
2021-10-19 13:57:39 -0400

    Add files via upload

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+from webdriver_manager.chrome import ChromeDriverManager 
+
+
+def scrape():
+
+executable_path = {'executable_path': ChromeDriverManager().install()}
+browser = Browser('chrome', **executable_path, headless=False)
+mars_url = 'https://redplanetscience.com/'
+news = requests.get(mars_url)
+browser.visit(mars_url)
+
+browser.visit(mars_url)
+html = browser.html
+bsoup = bs(html, 'html.parser')
+
+
+news_title = "NASA's Next Mars Mission to Investigate Interior of Red Planet"
+featured_image_url = 'https://spaceimages-mars.com/'
+browser.visit(featured_image_url)
+
+fact_url = 'https://galaxyfacts-mars.com/'
+browser = Browser('chrome', **executable_path, headless=False)
+browser.visit(fact_url)
+fact_table = browser.html
+fact_db = pd.read_html(fact_table, header=0)
+fact_df = fact_db[0]
+
+hemis_url = 'https://marshemispheres.com/'
+browser = Browser('chrome', **executable_path, headless=False)
+browser.visit(hemis_url)
+hemis_html = browser.html
+hsoup = bs(hemis_html, 'html.parser')
+
+hemis_image_url = 'https://marshemispheres.com/'
+browser.visit(hemis_image_url)
+
+
